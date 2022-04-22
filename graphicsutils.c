72ceb502f011c960106ee29ce923232b2dadc570
LucAlexander <lucskyalex@gmail.com>
2022-04-22 19:32:52 -0400

    render target changing

--- graphicsutils.c
+++ graphicsutils.c
@@ -182,6 +182,10 @@ void renderSetColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a){
 	SDL_SetRenderDrawColor(ghandle.renderer, r, g, b, a);
 }
 
+void renderSetTarget(SDL_Texture* t){
+	SDL_SetRenderTarget(ghandle.renderer, t);
+}
+
 void formatDestRectToView(SDL_Rect* destRect){
 	destRect->x-=ghandle.renderView.x;
 	destRect->y-=ghandle.renderView.y;
--- graphicsutils.h
+++ graphicsutils.h
@@ -85,6 +85,7 @@ void renderFlip();
 void renderClear();
 
 void renderSetColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+void renderSetTarget(SDL_Texture* t);
 
 void formatDestRectToView(SDL_Rect* destRect);
 void formatDestFRectToView(SDL_FRect* destRect);
